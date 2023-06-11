6764c6f19650318d11611ee8a2d5ab7e5e0d65a6
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2023-06-11 14:14:25 -0400

    chapter 24

--- CLox/vm.c
+++ CLox/vm.c
@@ -213,6 +213,11 @@ static InterpretResult run() {
 				if (isFalsey(peek(0))) vm.ip += offset;
 				break;
 			}
+			case OP_LOOP: {
+				uint16_t offset = READ_SHORT();
+				vm.ip -= offset;
+				break;
+			}
 			case OP_RETURN: {
 				// Exit interpreter for now
 				return INTERPRET_OK;
