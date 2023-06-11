5e0c04c5741b0e5469a51cd52ecb7ef6269b6cee
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2023-06-11 13:43:59 -0400

    chapter 23.3

--- CLox/chunk.h
+++ CLox/chunk.h
@@ -10,6 +10,8 @@ typedef enum {
 	OP_TRUE,
 	OP_FALSE,
 	OP_POP,
+	OP_GET_LOCAL,
+	OP_SET_LOCAL,
 	OP_GET_GLOBAL,
 	OP_SET_GLOBAL,
 	OP_DEFINE_GLOBAL,
@@ -23,6 +25,8 @@ typedef enum {
 	OP_NOT,
 	OP_NEGATE,
 	OP_PRINT,
+	OP_JUMP,
+	OP_JUMP_IF_FALSE,
 	OP_RETURN, // Ex. [01] <- opcode (1 byte)
 } OpCode;
 
